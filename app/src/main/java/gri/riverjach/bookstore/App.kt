ecc8ef17e30a72d87f866a706c814049d362a194
RIVIERE <gregory.riviere@laposte.net>
2020-06-05 10:37:08 +0200

    BookStore Test Timber pour les logs

--- app/src/main/java/gri/riverjach/bookstore/App.kt
+++ app/src/main/java/gri/riverjach/bookstore/App.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.room.Room
 import gri.riverjach.bookstore.database.AppDatabase
 import gri.riverjach.bookstore.database.DATABASE_NAME
+import timber.log.Timber
 
 class App : Application() {
     companion object {
@@ -12,12 +13,14 @@ class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
         db = Room.databaseBuilder(
             this,
             AppDatabase::class.java, DATABASE_NAME
         )
             .build()
+        Timber.d("Test")
     }
-
 
 }
