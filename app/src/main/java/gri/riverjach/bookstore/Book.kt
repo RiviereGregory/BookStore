1cb8178915b8e0fd835d78bc8d65e12974651891
RIVIERE <gregory.riviere@laposte.net>
2020-06-05 09:47:27 +0200

    first commit

---
+++ app/src/main/java/gri/riverjach/bookstore/Book.kt
@@ -0,0 +1,17 @@
+package gri.riverjach.bookstore
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+class Book(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var title: String,
+    var author: String,
+    var summary: String,
+    @ColumnInfo(name = "picture_url")
+var pictureUrl: String
+
+)
---
+++ app/src/main/java/gri/riverjach/bookstore/BooksListActivity.kt
@@ -0,0 +1,11 @@
+package gri.riverjach.bookstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BooksListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+    }
+}
