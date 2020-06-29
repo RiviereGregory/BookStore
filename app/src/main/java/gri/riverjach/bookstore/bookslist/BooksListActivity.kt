d754ac8ff78b099c0b8de4b60d04b37527f70bf8
RIVIERE <gregory.riviere@laposte.net>
2020-06-29 18:11:43 +0200

    BookStore Ajout listes de livres sur l'activity et mise en place du refraiche

--- app/src/main/java/gri/riverjach/bookstore/bookslist/BooksListActivity.kt
+++ app/src/main/java/gri/riverjach/bookstore/bookslist/BooksListActivity.kt
@@ -1,12 +1,50 @@
 package gri.riverjach.bookstore.bookslist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import gri.riverjach.bookstore.Book
 import gri.riverjach.bookstore.R
+import kotlinx.android.synthetic.main.activity_books.*
+import timber.log.Timber
 
-class BooksListActivity : AppCompatActivity() {
+class BooksListActivity : AppCompatActivity(), BooksListAdapter.BooksListAdapterListener {
+    private lateinit var viewModel: BooksListViewModel
+    private lateinit var booksAdapter: BooksListAdapter
+    private lateinit var books: MutableList<Book>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books)
+
+        books = mutableListOf()
+        booksAdapter = BooksListAdapter(books, this)
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@BooksListActivity)
+            adapter = booksAdapter
+        }
+        swipeRefresh.setOnRefreshListener{
+            viewModel.refreshBooks()
+        }
+
+        viewModel = ViewModelProvider(this).get(BooksListViewModel::class.java)
+        viewModel.books.observe(this, Observer { newBooks -> updateBooks(newBooks!!) })
     }
+
+    private fun updateBooks(newBooks: List<Book>) {
+        Timber.d("List of newBooks $newBooks")
+        books.clear()
+        books.addAll(newBooks)
+        booksAdapter.notifyDataSetChanged()
+        swipeRefresh.isRefreshing = false
+    }
+
+    override fun onBookSelected(book: Book) {
+        TODO("Not yet implemented")
+    }
+
+
 }
--- app/src/main/java/gri/riverjach/bookstore/bookslist/BooksListViewModel.kt
+++ app/src/main/java/gri/riverjach/bookstore/bookslist/BooksListViewModel.kt
@@ -8,4 +8,7 @@ import gri.riverjach.bookstore.Book
 class BooksListViewModel : ViewModel() {
     val books: LiveData<List<Book>> = App.db.bookDao().getAllBooks()
 
+    fun refreshBooks() {
+        App.repository.SyncBooksNow()
+    }
 }
